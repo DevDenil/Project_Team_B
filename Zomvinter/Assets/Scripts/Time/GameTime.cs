bb007aa2db9a53c5c5e66b03f354cb085b2b0795
choi-m-09 <audrbchzl@gmail.com>
2022-04-13 19:15:12 +0900

    Ignore

---
+++ Zomvinter/Assets/Scripts/Time/GameTime.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTime : MonoBehaviour
+{
+    public Text TestTime;
+    public float TimeScale;
+    public struct time
+    {
+        public float Gametime;
+        public int day;
+    }
+    public time gameTime;
+    public int day;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameTime.day = 0;
+        gameTime.Gametime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        day = gameTime.day;
+        gameTime.Gametime += TimeScale * Time.deltaTime;
+        TestTime.text = gameTime.Gametime.ToString("F2");
+        if(gameTime.Gametime >= 24.0f)
+        {
+            gameTime.day++;
+            gameTime.Gametime = 0.0f;
+        }
+    }
+}
