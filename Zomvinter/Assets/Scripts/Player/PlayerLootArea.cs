ae2d77823bd22e3acacd4cca7a8d7a7910faf25c
Denil <74027129+DevDenil@users.noreply.github.com>
2022-04-18 07:23:15 +0900

    ItemPickupUI

--- Zomvinter/Assets/Scripts/Player/PlayerLootArea.cs
+++ Zomvinter/Assets/Scripts/Player/PlayerLootArea.cs
@@ -6,7 +6,11 @@ public class PlayerLootArea : MonoBehaviour
 {
     public List<GameObject> LootableItems = new List<GameObject>();
 
-    private LayerMask ItemLayerMask;
+    public LayerMask ItemLayerMask;
+
+    PickUpUI myPickUpUI = null;
+
+    GameObject InstPickupUI = null;
     /*-----------------------------------------------------------------------------------------------*/
     void Start()
     {
@@ -15,7 +19,13 @@ public class PlayerLootArea : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && LootableItems.Count != 0)
+        {
+            this.GetComponentInParent<Player>().AddItems(LootableItems[0]);
+            Destroy(LootableItems[0]);
+            LootableItems.RemoveAt(0);
+            Destroy(InstPickupUI);
+        }
     }
     /*-----------------------------------------------------------------------------------------------*/
     private void OnTriggerEnter(Collider other)
@@ -23,10 +33,15 @@ public class PlayerLootArea : MonoBehaviour
         if ((ItemLayerMask & (1 << other.gameObject.layer)) != 0)
         {
             LootableItems.Add(other.gameObject);
+
+            if(InstPickupUI == null) InstPickupUI = Instantiate(Resources.Load("UI/ItemPickupUI"), GameObject.Find("Canvas").transform) as GameObject;
+            myPickUpUI = InstPickupUI.GetComponent<PickUpUI>();
+            myPickUpUI.Initialize(other.GetComponent<Transform>().transform, 50.0f);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         LootableItems.Remove(other.gameObject);
+        Destroy(InstPickupUI);
     }
 }
---
+++ Zomvinter/Assets/Weapon_AKM.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_AKM : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+    //Assets/Prefabs/Resources
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
