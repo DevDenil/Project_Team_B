a04989964356125c4a6c29eff76a5291c3b97502
choi-m-09 <audrbchzl@gmail.com>
2022-04-21 20:04:19 +0900

    LootingUi

--- Zomvinter/Assets/Scripts/VerticalAction.cs
+++ Zomvinter/Assets/Scripts/VerticalAction.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class VerticalAction : MonoBehaviour
 {
+    public GameObject Content;
+    List<GameObject> Slot = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
