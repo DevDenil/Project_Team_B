4a75996c36c194eab4eda3d50e2a85fd6d50f0b6
PXPXPPX <103184001+PXPXPPX@users.noreply.github.com>
2022-06-07 22:53:07 +0900

    AddMap

---
+++ Zomvinter/Assets/Scripts/Player/RoofControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class RoofControl : MonoBehaviour
+{
+    public GameObject Roof;
+    public LayerMask Player;
+    private List<GameObject> PlayerCheck = new List<GameObject> ();
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TriggerEnter");
+        if((Player & (1<<other.gameObject.layer)) != 0)
+        {
+            PlayerCheck.Add(other.gameObject);
+            //Roof.SetActive(false);
+            if(PlayerCheck.Count > 1)
+            {
+                Roof.SetActive(false);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if ((Player & (1 << other.gameObject.layer)) != 0)
+        {
+            Roof.SetActive(true);
+        }
+    }
+}
